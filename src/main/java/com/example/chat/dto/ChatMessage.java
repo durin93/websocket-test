a527e05a5859a25b6e108fff58539146b131908e
gram.eu(이승철) <gram.eu@kakaomobility.com>
2019-08-17 17:49:31 +0900

    채팅방 인원수

--- src/main/java/com/example/chat/dto/ChatMessage.java
+++ src/main/java/com/example/chat/dto/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.example.chat.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,13 +10,28 @@ import lombok.ToString;
 @ToString
 public class ChatMessage {
 
+
+    public ChatMessage() {
+    }
+
+    @Builder
+    public ChatMessage(MessageType type, String roomId, String sender, String message, long userCount) {
+        this.type = type;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.message = message;
+        this.userCount = userCount;
+    }
+
+
     public enum MessageType {
-        ENTER, TALK
+        ENTER, QUIT, TALK
     }
 
     private MessageType type; // 메시지 타입
     private String roomId; // 방번호
     private String sender; // 메시지 보낸사람
     private String message; // 메시지
+    private Long userCount; // 채팅방 인원수, 채팅방 내에서 메시지가 전달될때 인원수 갱신시 사용
 }
 
--- src/main/java/com/example/chat/handler/ChatController.java
+++ src/main/java/com/example/chat/handler/ChatController.java
@@ -1,11 +1,11 @@
 package com.example.chat.handler;
 
 
+import com.example.chat.domain.ChatRoomRepository;
 import com.example.chat.dto.ChatMessage;
+import com.example.chat.service.ChatService;
 import com.example.chat.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
@@ -14,10 +14,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-
-    private final RedisTemplate<String, Object> redisTemplate;
     private final JwtTokenProvider jwtTokenProvider;
-    private final ChannelTopic channelTopic;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatService chatService;
 
     /**
      * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
@@ -27,13 +26,10 @@ public class ChatController {
         String nickname = jwtTokenProvider.getUserNameFromJwt(token);
         // 로그인 회원 정보로 대화명 설정
         message.setSender(nickname);
-        // 채팅방 입장시에는 대화명과 메시지를 자동으로 세팅한다.
-        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-            message.setSender("[알림]");
-            message.setMessage(nickname + "님이 입장하셨습니다.");
-        }
+        // 채팅방 인원수 세팅
+        message.setUserCount(chatRoomRepository.getUserCount(message.getRoomId()));
         // Websocket에 발행된 메시지를 redis로 발행(publish)
-        redisTemplate.convertAndSend(channelTopic.getTopic(), message);
+        chatService.sendChatMessage(message);
     }
 
 }
