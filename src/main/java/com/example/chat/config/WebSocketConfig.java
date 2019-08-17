9ff408fc37d04ae6994cc343dcfa8d16122e7928
gram.eu(이승철) <gram.eu@kakaomobility.com>
2019-08-17 17:30:53 +0900

    spring security & jwt setting

--- src/main/java/com/example/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/chat/config/WebSocketConfig.java
@@ -1,22 +1,20 @@
 package com.example.chat.config;
 
+import com.example.chat.handler.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @Configuration
-//@EnableWebSocket //webSocket 활성화
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-//public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final StompHandler stompHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -27,17 +25,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
-            .withSockJS();
+            .withSockJS(); // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
     }
-
-    /*
-    private final WebSocketHandler webSocketHandler;
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws/chat") //커넥션을 일로연결하 메세지통신가능하도록 설정
-            .setAllowedOrigins("*"); //cors 허용
-
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
-*/
+
 }
